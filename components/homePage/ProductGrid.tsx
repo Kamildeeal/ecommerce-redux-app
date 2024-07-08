28b27d64551371cc9112b44161cb9ee3a971c5b3
unknown <kamil.kamil.1995@wp.pl>
2024-07-08 11:16:38 +0200

    add inifite scroll page to display specified amount of product to prevent waiting for data, add Enum with Categories to modularize code

--- components/homePage/ProductGrid.tsx
+++ components/homePage/ProductGrid.tsx
@@ -1,22 +1,49 @@
 "use client";
 
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import ProductCard from "./ProductCard";
 import { Product } from "@/lib/types/types";
 import { useAppDispatch, useAppSelector } from "@/lib/hookts";
 import { RootState } from "@/lib/store";
 import Categories from "@/lib/enums/ProductCategories";
+import {
+  fetchProducts,
+  loadMoreProducts,
+} from "@/lib/features/products/FetchDataSlice";
 
 interface ProductGridProps {
   products: Product[];
 }
 
 const ProductGrid = ({ products }: ProductGridProps) => {
-  const { filteredProducts, categoryFilter } = useAppSelector(
-    (state: RootState) => state.products
+  const dispatch = useAppDispatch();
+  const { filteredProducts, categoryFilter, displayedProducts, loading, page } =
+    useAppSelector((state: RootState) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  const handleScroll = useCallback(
+    (e: any) => {
+      if (
+        window.innerHeight + e.target.documentElement.scrollTop + 1 >=
+        e.target.documentElement.scrollHeight
+      ) {
+        if (!loading) {
+          dispatch(loadMoreProducts());
+        }
+      }
+    },
+    [dispatch, loading]
   );
 
-  if (!products || products.length === 0) {
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [handleScroll]);
+
+  if (loading && displayedProducts.length === 0) {
     return (
       <div className="flex justify-center text-center w-full items-center mx-auto">
         Loading...
@@ -25,13 +52,16 @@ const ProductGrid = ({ products }: ProductGridProps) => {
   }
 
   const productsToDisplay =
-    categoryFilter === Categories.AllItems ? products : filteredProducts;
+    categoryFilter === Categories.AllItems
+      ? displayedProducts
+      : filteredProducts;
 
   return (
     <div className="grid justify-items-center sm:justify-normal grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-col-3 xl:grid-cols-4 lg:px-12 gap-6 py-6 px-6">
       {productsToDisplay.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
+      {loading && <p>Loading more products...</p>}
     </div>
   );
 };
--- lib/features/products/FetchDataSlice.ts
+++ lib/features/products/FetchDataSlice.ts
@@ -1,5 +1,3 @@
-// @/features/products/productsSlice.ts
-
 import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 
@@ -27,7 +25,9 @@ interface FetchData {
   products: Product[];
   filteredProducts: Product[];
   categoryFilter: string | null;
+  displayedProducts: Product[];
   error: null | string;
+  page: number;
 }
 
 export const fetchProducts = createAsyncThunk(
@@ -40,14 +40,14 @@ export const fetchProducts = createAsyncThunk(
   }
 );
 
-//https://dummyjson.com/products?limit=100
-
 const initialState: FetchData = {
   loading: true,
   products: [],
   filteredProducts: [],
-  categoryFilter: "fragrances",
+  categoryFilter: "allItems",
+  displayedProducts: [],
   error: null,
+  page: 1,
 };
 
 const productsSlice = createSlice({
@@ -56,18 +56,30 @@ const productsSlice = createSlice({
   reducers: {
     setCategoryFilter: (state, action: PayloadAction<string | null>) => {
       state.categoryFilter = action.payload;
-      if (state.categoryFilter) {
+      if (state.categoryFilter === "allItems") {
+        state.filteredProducts = state.products;
+      } else {
         state.filteredProducts = state.products.filter(
           (product) => product.category === state.categoryFilter
         );
-      } else {
-        state.filteredProducts = state.products;
       }
+      state.page = 1;
+      state.displayedProducts = state.filteredProducts.slice(0, 16);
     },
     sortProductsByCategory: (state) => {
       state.filteredProducts = [...state.filteredProducts].sort((a, b) =>
         a.category.localeCompare(b.category)
       );
+      state.displayedProducts = state.filteredProducts.slice(
+        0,
+        state.page * 16
+      );
+    },
+    loadMoreProducts: (state) => {
+      const start = state.page * 16;
+      const moreProducts = state.filteredProducts.slice(start, start + 16);
+      state.displayedProducts = [...state.displayedProducts, ...moreProducts];
+      state.page += 1;
     },
   },
   extraReducers: (builder) => {
@@ -78,6 +90,8 @@ const productsSlice = createSlice({
       .addCase(fetchProducts.fulfilled, (state, action) => {
         state.loading = false;
         state.products = action.payload;
+        state.filteredProducts = action.payload;
+        state.displayedProducts = action.payload.slice(0, 16);
       })
       .addCase(fetchProducts.rejected, (state, action) => {
         state.loading = false;
@@ -85,6 +99,6 @@ const productsSlice = createSlice({
       });
   },
 });
-export const { setCategoryFilter, sortProductsByCategory } =
+export const { setCategoryFilter, sortProductsByCategory, loadMoreProducts } =
   productsSlice.actions;
 export default productsSlice.reducer;
